d3b6839bcb0f72117d12544ed50616fced397e21
Nate Taylor <nate@rtelectronix.com>
2020-04-07 11:28:36 -0500

    Added index and root typing to the project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { SmartTable } from "./SmartTable";
+import { TableColumn } from "./TableColumn";
+import { TableData } from "./TableData";
+import { TableParser } from "./TableParser";
+import { TableRow } from "./TableRow";
+
+export {SmartTable, TableColumn, TableData, TableParser, TableRow};
